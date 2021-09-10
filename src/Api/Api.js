73886497e751de5202b9efbe75a2f5b4568bb529
Marcus Pontes <marcusporama@gmail.com>
2021-09-09 21:38:54 -0300

    update api for https in baseUrl

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
-const api = axios.create({ baseURL: "http://api.icndb.com/jokes/random/" })
+const api = axios.create({ baseURL: "https://api.icndb.com/jokes/random/" })
 
 export default api
